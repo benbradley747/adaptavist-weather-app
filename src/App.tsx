f2fc0e4014c7166febf99ad8a13a299074c367de
Benjamin Bradley <bbradley@Benjamins-MacBook-Pro.local>
2022-12-26 16:29:16 -0600

    feat: setup search component
    - linked search component to redux store
    
     - added background image
    
     -
    import cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { useDispatch } from "react-redux";
+import { CircularProgress } from "@mui/material";
 import "./App.css";
-import { Button } from "@mui/material";
-import { fetchWeatherData } from "./store/actions/WeatherActions";
-import { useAppDispatch, useAppSelector } from "./store/hooks";
+import Search from "./components/Search"
+import { useAppSelector } from "./store/hooks";
 
 const App = () => {
-  const dispatch = useAppDispatch();
-
-  const fetchData = async () => {
-    dispatch(fetchWeatherData("Chicago"));
-  };
-
   return (
     <div className="App">
-      <p>Hello World</p>
-      <Button onClick={fetchData}>Fetch weather data</Button>
+      <Search/>
     </div>
   );
 };
---
+++ src/components/Search.tsx
@@ -0,0 +1,23 @@
+import { Button, TextField } from "@mui/material"
+import React, { useState } from "react";
+import { fetchWeatherData } from "../store/actions/WeatherActions";
+import { useAppDispatch } from "../store/hooks"
+
+const Search = () => {
+    const dispatch = useAppDispatch();
+    const [query, setQuery] = useState<string>("");
+
+    const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        dispatch(fetchWeatherData(query))
+    }
+
+    return (
+        <div>
+            <TextField variant="outlined" placeholder="Search" onChange={(e) => setQuery(e.target.value)}/>
+            <Button variant="contained" color="success" onClick={(e) => handleSubmit(e)}>Get Weather</Button>
+        </div>
+    )
+}
+
+export default Search
